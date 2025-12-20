da988f2a099485488ada7563be990c72a78c51f9
Yugo l'Eliatrop <papasergiusv@gmail.com>
2025-12-20 22:06:48 +0100

    fix parser, add layout

--- src/parser.ts
+++ src/parser.ts
@@ -1,13 +1,13 @@
 import { ParsedData } from './types';
 
-const SENSOR_DATA_REGEX = /^t:(-?\d+(?:\.\d+)?),\sh:(-?\d+(?:\.\d+)?)$/;
+const SENSOR_DATA_REGEX = /^t:(-?\d+(?:\.\d+)?),h:(-?\d+(?:\.\d+)?)$/;
 
 export const parseSerialData = (data: Buffer): ParsedData => {
-  const str = data.toString().replace(/\r|\n/g, '');
+  const str = data.toString().replace(/\r|\n|\s/g, '');
   const match = str.match(SENSOR_DATA_REGEX);
 
   if (!match) {
-    return str;
+    return data.toString().replace(/\r|\n/g, '');
   }
 
   return {
